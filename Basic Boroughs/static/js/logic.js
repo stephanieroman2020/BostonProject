068f7bfccb41d3f58e454344fb320effc2474ef3
Stephanie Roman <56005977+stephanieroman2020@users.noreply.github.com>
2020-03-06 17:33:53 -0500

    Add files via upload
    Boston Neighborhood Map

---
+++ Basic Boroughs/static/js/logic.js
@@ -0,0 +1,22 @@
+// Creating map object
+var map = L.map("map", {
+  center: [42.3601, -71.0589],
+  zoom: 11
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(map);
+
+// Uncomment this link local geojson for when data.beta.nyc is down
+var link = "static/data/Boston.geojson";
+
+// Grabbing our GeoJSON data..
+d3.json(link, function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(map);
+});
