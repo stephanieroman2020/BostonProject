6a3073d984bae6dc0578080d3ebc19a497994fcf
Victoria Edwards <victoriaedwards22@icloud.com>
2020-03-12 12:35:55 -0400

    server and db

---
+++ BostonFlaskServer.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import sqlite3
+import pandas as pd
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('webpage_d3 - 3.0.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+@app.route('/trash', methods=('GET', 'POST'))
+def trash():
+    connection = sqlite3.connect("boston.db")
+    if request.method == 'GET':
+        df3 = pd.read_sql_query("select * from TrashTable limit 5;", connection)
+       
+
+    return df3.to_json()
+
