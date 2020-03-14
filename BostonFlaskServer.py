e464808f9efcb1466121f6647ce6bc28a3f73341
Victoria Edwards <victoriaedwards22@icloud.com>
2020-03-13 22:14:56 -0400

    5.0

--- BostonFlaskServer.py
+++ BostonFlaskServer.py
@@ -2,25 +2,38 @@ from flask import Flask, render_template
 import sqlite3
 import pandas as pd
 from flask import request
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('webpage_d3 - 3.0.html')
+    return render_template('webpage_consolidated - 5.0.html')
+
+@app.route('/trash')
+def trash():
+    trash_file=open("static/data/Trash.geojson", 'r')
+    trash_json=json.load(trash_file)
+    return json.dumps(trash_json)
+
+@app.route('/noise')
+def noise():
+    noise_file=open("static/data/Noise2.geojson", 'r')
+    noise_json=json.load(noise_file)
+    return json.dumps(noise_json)
+
+@app.route('/rodents')
+def rodents():
+    rodents_file=open("static/data/RodentsPests.geojson", 'r')
+    rodents_json=json.load(rodents_file)
+    return json.dumps(rodents_json)
+
+@app.route('/neighborhoods')
+def neighborhoods():
+    neighborhoods_file=open("static/data/Boston_Neighborhoods.geojson", 'r')
+    neighborhoods_json=json.load(neighborhoods_file)
+    return json.dumps(neighborhoods_json)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-@app.route('/trash', methods=('GET', 'POST'))
-def trash():
-    connection = sqlite3.connect("boston.db")
-    if request.method == 'GET':
-        df3 = pd.read_sql_query("select * from TrashTable limit 5;", connection)
-       
-    return render_template('webpage_d3 - 2.1.html')
-   #return df3.to_json()
-
--- Frontend_5.0/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1Ijoic3RlcGhhbmllcm9tYW4yMDIwIiwiYSI6ImNrNzQwbXpoODA4bmczdnBvbjc4NGp6NzMifQ.D-M7rV4Ym1A1fbRU_TOpzA";
